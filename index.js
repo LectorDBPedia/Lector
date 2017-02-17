facd87cf90e6f8357e53755453bee0ff9fc00854
gabmarini <gab.marini2@gmail.com>
2017-02-17 17:46:10 +0100

    added paper

--- index.js
+++ index.js
@@ -6,26 +6,39 @@ var relTriples = require('./schemas/rel_triples');
 var trustedTriples = require('./schemas/trusted_triples')
 var untrustedTriples = require('./schemas/untrusted_triples')
 var entityTypes = require('./schemas/entity_types')
+var relationPhrases = require('./schemas/relation_phrase');
+var unlabeledTriples = require('./schemas/unlabeled_triples')
+var labeledTriples = require('./schemas/labeled_triples')
 
 connection.on('connected', () => {
 
-/*esecuzione dei task in parallelo, ogni res viene messo in un array result finale*/
-	async.parallel([
-	function(callback){
-		relTriples.findOne({}, (err, res) => {callback(err, res)})
-	},
-	function(callback){
-		trustedTriples.findOne({}, (err, res) => {callback(err, res)})
-	},
-	function(callback){
-		untrustedTriples.findOne({}, (err, res) => {callback(err, res)})
-	},
-	function(callback){
-		entityTypes.findOne({}, (err, res) => {callback(err, res)})
-	}], (err, result) => { //funzione eseguita alla fine di tutte le funzioni sopra
-		
-		console.log(result)
-		connection.close(() => {console.log('connection closed, task completed')})
-	
-	});
+
+	var stream = relTriples.find({}).stream();
+	stream.on('data', (doc) => {
+
+		trustedTriples.find({'subject': doc.subject, 'object' : doc.object}, (err, res) =>{
+			if (!res.length){
+				unlabeledTriples.create(doc._doc, (err, res) => {}) //loggare in qualche modo
+			} else {
+				const entriesPromises = Promise.all([res.map((r) => {
+					return Promise.all([
+						labeledTriples.create({'subject': doc.subject, 'phrase': doc.phrase, 'object': doc.object, 'relation': r.relation}),
+						relationPhrases.findOne({'relation' : r.relation, 'phrase': doc.phrase})
+						]).then(([_, resultFindOne]) => {
+							return relationPhrases.update({"relation": r.relation, "phrase": doc.phrase}, {$inc: {"count": 1}}, {upsert: true})
+						})
+					})]);
+				entriesPromises.then(() => {
+				})
+				.catch(console.error);
+			}
+		})
+
+	})
+
+	stream.on('close', () => {
+		connection.close( () => { 
+			console.log('connection closed, task completed, or error')
+		})
+	})
 })
