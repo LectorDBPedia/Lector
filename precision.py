f15ff47ca0267baa557983df67f3964065c6d02f
Marco Faretra <mar.faretra@stud.uniroma3.it>
2017-02-23 15:54:49 +0100

    fix precision

--- precision.py
+++ precision.py
@@ -6,12 +6,10 @@ with open(sys.argv[1], "rb") as f:
 	countAll = 0
 	count = 0
 	for row in reader:
-		try:
-			if row[5]:
-				if row[1] == sys.argv[2]:
-					countAll += 1
-					if row[5] == 'T':
-						count += 1
-		except IndexError:
-			break
+		if len(row) == 6:
+			if row[1] == sys.argv[2]:
+				countAll += 1
+				if row[5] == 'T':
+					count += 1
+
 	print "totale vere " + str(count) + " su un totale di " + str(countAll) + " con precision = " + str((float(count)/float(countAll)) * 100) + "%"
